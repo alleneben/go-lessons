2e5d6e94d2d84e164634b9194674c5b334ca8519
Allen Eben <allen.eben@fjorgedigital.com>
2023-09-17 20:41:50 +0000

    :arrays

--- main.go
+++ main.go
@@ -4,5 +4,16 @@ import "fmt";
 
 
 func main() {
-	fmt.Println("Hello Allen")
+	//Arrays
+	var ages [4]int = [4]int{3, 5, 7, 6}
+
+	var new_ages = [6]int{3, 5, 7, 6, 2, 2}
+
+	names := [4]string{"allen", "eben", "ethereum", "nana"}
+
+	fmt.Println(new_ages, len(new_ages)) 
+	fmt.Println(ages, len(ages))
+	fmt.Println(names)
+
+	//Slices
 }
